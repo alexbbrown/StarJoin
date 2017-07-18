e889db650a500eec479310538839a9f2c1c588e0
Alex Brown <alexbbrown@apple.com>
2017-07-18 11:47:04 -0700

    Added empty adaptor project for spritekit

---
+++ Adaptors/StarJoinSpriteKitAdaptor/StarJoinSpriteKitAdaptor/StarJoinSpriteKitAdaptor.h
@@ -0,0 +1,19 @@
+//
+//  StarJoinSpriteKitAdaptor.h
+//  StarJoinSpriteKitAdaptor
+//
+//  Created by apple on 7/18/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StarJoinSpriteKitAdaptor.
+FOUNDATION_EXPORT double StarJoinSpriteKitAdaptorVersionNumber;
+
+//! Project version string for StarJoinSpriteKitAdaptor.
+FOUNDATION_EXPORT const unsigned char StarJoinSpriteKitAdaptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarJoinSpriteKitAdaptor/PublicHeader.h>
+
+

c59dba3a378684fcbdd0483cb9ad15bfa75e1831
Alex Brown <alexbbrown@apple.com>
2017-07-18 02:44:45 -0700

    First code that exercises select: boom! : fatal error: Can't form Range with upperBound < lowerBound

--- Development Playgrounds/SelectEditing.playground/Contents.swift
+++ Development Playgrounds/SelectEditing.playground/Contents.swift
@@ -1265,3 +1265,122 @@ public func allChildrenNamedSelector(name:String) -> (_:SKNode) -> [SKNode] {
 
 
 1+1
+
+import SpriteKit
+
+typealias TableRow = [String:Any]
+
+var emptyData = [TableRow]()
+var oneRowData:[TableRow] = [["x": 1.0, "y": 2.0]]
+
+let scene = SKScene(size: CGSize(width: 1024, height: 768))
+
+
+func testSelectionConstructionZero() {
+    // This is an example of a functional test case.
+
+    let children = scene.children
+
+    let mySelection = Selection<SKNode>.selection(parent: scene, nodes: scene.childNodes, data: emptyData)
+
+    assert(0 == mySelection.nodes.count)
+    assert(0 == mySelection.data.count)
+}
+
+testSelectionConstructionZero()
+
+func testSelectionConstructionOne() {
+    // This is an example of a functional test case.
+
+    let children = scene.children
+
+    let mySelection = Selection<SKNode>.selection(parent: scene, nodes: scene.childNodes, data: oneRowData)
+
+    assert(0 == mySelection.nodes.count) // the join has NO nodes to begin with (before enter)
+    assert(1 == mySelection.data.count)
+
+    // defunct - we don't do it that way any more
+    //        let nonNilCount = mySelection.nodes.reduce(0, combine: { (acc, elem) -> Int in
+    //            if let elemReal = elem {
+    //                return acc + 1
+    //            } else {
+    //                return acc
+    //            }
+    //        })
+    //
+    //        XCTAssertEqual(0, nonNilCount);
+
+}
+
+testSelectionConstructionOne()
+
+#if false
+
+    func testSelectionOneEnter() {
+        // This is an example of a functional test case.
+
+        let children = scene?.children
+
+        let mySelection = Selection<SKNode>.selection(scene!, nodes: scene!.childNodes, data: self.oneRowData)
+
+        mySelection.enter().append { (s, d, i) -> SKNode in
+            return SKNode()
+        }
+
+        XCTAssertEqual(0, mySelection.nodes.count)
+        XCTAssertEqual(1, mySelection.data.count)
+        XCTAssertEqual(1, scene!.children.count)
+
+        // defunct - we don't do it that way any more
+        //        let nonNilCount = mySelection.nodes.reduce(0, combine: { (acc, elem) -> Int in
+        //            if let elemReal = elem {
+        //                return acc + 1
+        //            } else {
+        //                return acc
+        //            }
+        //        })
+        //
+        //        XCTAssertEqual(1, nonNilCount);
+
+        XCTAssert(true, "Pass")
+    }
+
+    func testSelectionOneZeroEnterExit() {
+        // This is an example of a functional test case.
+
+        let children = scene?.children
+
+        // If the node has children my code fails.
+
+        let mySelection = Selection<SKNode>.selection(scene!, nodes: scene!.childNodes, data: self.oneRowData)
+
+        mySelection.enter().append { (s, d, i) -> SKNode in
+            return SKNode()
+        }
+
+        let mySelection2 = Selection<SKNode>.selection(scene!, nodes: scene!.childNodes, data: self.emptyData)
+
+        mySelection2.enter().append { (s, d, i) -> SKNode in
+            return SKNode()
+        }
+
+        mySelection2.exit().remove()
+
+        XCTAssertEqual(0, mySelection2.nodes.count) // the selection array gets initialised with nil optionals.
+        XCTAssertEqual(0, mySelection2.data.count)
+        XCTAssertEqual(0, scene!.children.count) // will fail until exit is implemented
+
+        //        let nonNilCount = mySelection2.selection.reduce(0, combine: { (acc, elem) -> Int in
+        //            if let elemReal = elem {
+        //                return acc + 1
+        //            } else {
+        //                return acc
+        //            }
+        //        })
+        //
+        //        XCTAssertEqual(0, nonNilCount);
+
+        XCTAssert(true, "Pass")
+    }
+#endif
+

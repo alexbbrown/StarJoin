430bb661a308cfbebdffd73bd4009b7d5721abed
Alex Brown <alexbbrown@apple.com>
2017-07-19 13:08:08 -0700

    Added missing file

---
+++ Example Playgrounds/SpriteKit Tutorial/Joining to Data.playground/Pages/2.4.2 Change forever quickly.xcplaygroundpage/Sources/ui.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SpriteKit
+
+// The update function is in this file because the code in the sources directory of playgrounds is run at a much faster speed than that in the main view.  It would be even faster in an application
+public func updateNode(s:SKNode?, d:TableRow?, i:Int) -> () {
+    if let sprite = s as? SKSpriteNode {
+        sprite.position = CGPoint(x: CGFloat(d!.x), y: CGFloat(d!.y))
+        sprite.color = colors.color(withKey:d!.color) ?? .red
+        sprite.size = CGSize(width: CGFloat(d!.size), height: CGFloat(d!.size))
+    }
+}

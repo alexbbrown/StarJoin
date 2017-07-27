03c0e0bb93daf9d1b922313242f5da42e058cbbf
Alex Brown <alexbbrown@apple.com>
2017-07-26 22:23:20 -0700

    Added new playground moving towards NSView

--- Example Playgrounds/Tutorial/Joining to Data.playground/Pages/1.3a Serious Structs to NSView.xcplaygroundpage/Contents.swift
+++ Example Playgrounds/Tutorial/Joining to Data.playground/Pages/1.3a Serious Structs to NSView.xcplaygroundpage/Contents.swift
@@ -1,28 +1,23 @@
 //: [Previous-Joining Dictionaries](@previous)
-//:# Structs to Sprites
+//:# Structs to UIView
 import StarJoinSelector
-import StarJoinSpriteKitAdaptor
-import SpriteKit
+import StarJoinUIViewAdaptor
+import UIKit
 /*:
  Enable SpriteKit for Playground
  */
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+var sceneView = UIView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+let scene = sceneView
 
-// Add a spritekit window to the Live View
+// Add a UIKit View to the Live View
 import PlaygroundSupport
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-// Create the scene and add it to the view
-let scene:SKScene = SKScene(size: CGSize(width:640, height:480))
-scene.scaleMode = .resizeFill
-sceneView.presentScene(scene)
-
 //: **More Serious Data**
 //:
 //: This example uses an array of typed structs - we could easily use classes too.  You could even use any class in Cocoa or other APIs.
 struct RowStruct {
     let position : CGPoint
-    let color : SKColor
+    let color : UIColor
     let size : CGSize
 }
 
@@ -33,8 +28,8 @@ var nodeArray = [
 ]
 //: **Selection** picks a root node and 'joins' it to the data
 
-let mySelection = select(node:scene as SKNode)
-    .select(all: allChildrenSelector)
+let mySelection = select(node:scene as UIView)
+    .select(all: scene.childNodes)
     .join(nodeArray)
 
 //: **enter** focuses on the new nodes we need,
@@ -42,8 +37,14 @@ let mySelection = select(node:scene as SKNode)
 //: **attr** sets sprite properties using the **struct** data value `d`.  `s` is the sprite, which can be useful.
 mySelection
     .enter()
-    .append { (_, _) in SKSpriteNode() }
-    .attr("position") { (s, d, i) in d.position }
-    .attr("size") { (s, d, i) in d.size }
-    .attr("color") { (s, d, i) in d.color }
+    .append { (_, _) in UIButton(type:.system) }
+    .attr("frame") { (s, d, i) in CGRect(origin:d.position, size:d.size) }
+    .attr("backgroundColor") { (s, d, i) in d.color }
+    .each { (s, d, i) in
+        (s as? UIButton)?.setTitle("🐞", for: .normal)
+
+    .attr("showsTouchWhenHighlighted") { (s, d, i) in true }
+    .attr("layer.cornerRadius") { (s, d, i) in d.size.width / 4 }
+
 //: [Next–Section 2–evolving data](@next)
+

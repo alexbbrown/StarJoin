5b134da70e4a12bb0dc0b5c0a8f1d7dd85c9e59d
Alex Brown <alexbbrown@apple.com>
2017-07-26 22:25:01 -0700

    Added UIKit simple tests

--- Adaptors/StarJoinUIViewAdaptor/StarJoinUIViewAdaptor/UIViewAdaptor.swift
+++ Adaptors/StarJoinUIViewAdaptor/StarJoinUIViewAdaptor/UIViewAdaptor.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import SpriteKit
+import StarJoinSelector
+import CoreGraphics.CGGeometry
+
+// The following should really be in a SKNode swift module for adapting this protocol to SKNode
+extension UIView: TreeNavigable {
+
+    final public var childNodes: [UIView] {
+        get { return self.subviews }
+    }
+
+    public func remove(child: UIView) {
+        child.removeFromSuperview()
+    }
+
+    public func add(child: UIView) {
+        self.addSubview(child)
+    }
+}
+
+extension UIView: NodeMetadata {
+
+    struct Static {
+        static var key = "SJmetadata"
+    }
+    public var metadata:Any? {
+        get {
+            return objc_getAssociatedObject( self, &Static.key ) as Any?
+        }
+        set {
+            objc_setAssociatedObject( self, &Static.key,  newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+}
+
+extension UIView: KVC {
+
+    public func setNodeValue(_ toValue:Any?, forKeyPath keyPath:String)
+    {
+        if let toValue = toValue {
+            self.setValue(toValue, forKeyPath: keyPath)
+        }
+    }
+}
+
--- Adaptors/StarJoinUIViewAdaptor/StarJoinUIViewAdaptorTests/StarJoinUIViewAdaptorTests.swift
+++ Adaptors/StarJoinUIViewAdaptor/StarJoinUIViewAdaptorTests/StarJoinUIViewAdaptorTests.swift
@@ -7,13 +7,25 @@
 //
 
 import XCTest
+import UIKit
+@testable import StarJoinSelector
 @testable import StarJoinUIViewAdaptor
 
 class StarJoinUIViewAdaptorTests: XCTestCase {
-    
+
+    typealias TableRow = [String:Any]
+
+    // empty data set
+    var emptyData = [TableRow]()
+    var oneRowData:[TableRow] = [["x":1.0, "y":2.0]]
+
+    var root:UIView? = nil;
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        root = UIView(frame:CGRect(x:0, y:0, width: 1024, height: 768))
+
     }
     
     override func tearDown() {
@@ -32,5 +44,51 @@ class StarJoinUIViewAdaptorTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
+    func testSelectionConstructionZero() {
+        // This is an example of a functional test case.
+
+        let mySelection = StarJoin.select(only: root!).select(all: root!.childNodes).join(emptyData)
+
+        XCTAssertEqual(0, mySelection.debugNodes.count)
+        XCTAssertEqual(0, mySelection.debugNewData.count)
+
+        XCTAssert(true, "Pass")
+    }
+
+    func testSelectionConstructionOne() {
+        // This is an example of a functional test case.
+
+        let mySelection = StarJoin.select(only: root!).select(all: root!.childNodes).join(oneRowData)
+
+        XCTAssertEqual(0, mySelection.debugNodes.count) // the join has NO nodes to begin with (before enter)
+        XCTAssertEqual(1, mySelection.debugNewData.count)
+
+        XCTAssert(true, "Pass")
+    }
+
+    func testSelectionOneEnter() {
+        // This is an example of a functional test case.
+
+        let mySelection = StarJoin.select(only: root!).select(all: root!.childNodes).join(self.oneRowData)
+
+        XCTAssertEqual(0, mySelection.debugNodes.count)
+        XCTAssertEqual(1, mySelection.debugNewData.count)
+
+        let enterSelection = mySelection.enter()
+
+        XCTAssertEqual(0, mySelection.debugNodes.count)
+
+        enterSelection.append { (d, i) -> UIView in
+            return UIView()
+        }
+
+        XCTAssertEqual(0, mySelection.debugNodes.count) // used to be 1
+        XCTAssertEqual(1, mySelection.debugNewData.count)
+        XCTAssertEqual(1, root!.childNodes.count)
+
+        XCTAssert(true, "Pass")
+    }
+
     
 }

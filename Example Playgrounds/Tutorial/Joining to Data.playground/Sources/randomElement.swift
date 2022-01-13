66c70576e54023926b832a8f16a58cff087fa51e
Alex Brown <alexbbrown@apple.com>
2022-01-13 11:26:48 -0800

    Fix warnings

--- Example Playgrounds/Tutorial/Joining to Data.playground/Sources/randomElement.swift
+++ Example Playgrounds/Tutorial/Joining to Data.playground/Sources/randomElement.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public extension CountableRange {
     /// return a random element of the range using arc4random
-    public func randomElement() -> Bound {
+    func randomElement() -> Bound {
         return self.lowerBound.advanced(by: Int(arc4random_uniform(UInt32(self.lowerBound.distance(to: self.upperBound)))) as! Bound.Stride)
     }
 }
 
 public extension Array {
     /// return a random element of the array using arc4random
-    public func randomElement() -> Element {
+    func randomElement() -> Element {
         return self[indices.randomElement()]
     }
 }

a515d502146f7af65241c3d305dd99916c8bee7e
Alex Brown <alexbbrown@apple.com>
2022-01-13 11:51:00 -0800

    Remade StarJoinSceneKitAdaptor

--- StarJoinSceneKitAdaptor/Sources/StarJoinSceneKitAdaptor/StarJoinSceneKitAdaptor.swift
+++ StarJoinSceneKitAdaptor/Sources/StarJoinSceneKitAdaptor/StarJoinSceneKitAdaptor.swift
@@ -1,6 +1,39 @@
-public struct StarJoinSceneKitAdaptor {
-    public private(set) var text = "Hello, World!"
+//
+//  StarJoinSceneKitAdaptor.swift
+//  StarJoinSceneKitAdaptor
+//
+//  Created by apple on 7/20/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
 
-    public init() {
+import Foundation
+import SceneKit
+import StarJoinSelector
+
+extension SCNNode: TreeNavigable, NodeMetadata, KVC {
+
+    public func remove(child: SCNNode) {
+        child.removeFromParentNode()
+    }
+
+    public func add(child: SCNNode) {
+        self.addChildNode(child)
+    }
+
+//    public func removeNodeFromParent() {
+//        self.removeFromParentNode()
+//    }
+
+    public func setNodeValue(_ toValue: Any?, forKeyPath keyPath: String) {
+        if let toValue = toValue {
+            self.setValue(toValue, forKeyPath: keyPath)
+        }
+    }
+
+    public var metadata: Any? {
+        get { return self.value(forKey: "metadata") }
+        set(value) {
+            self.setValue(value, forKey: "metadata")
+        }
     }
 }

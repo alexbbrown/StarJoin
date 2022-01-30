c438b177ec1e7de44211962187d27d05b9130980
Alex Brown <alexbbrown@apple.com>
2022-01-29 17:54:54 -0800

    added troubleshooting playground, for playgrounds related notes

---
+++ Example Playgrounds/Tutorial/Joining to Data.playground/Pages/TroubleShooting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+
+ # Recommendations
+
+ For each playground, set the scheme to the adaptor that playground uses
+
+ Set execution mode to automatic
+
+ # TroubleShooting Playgrounds: Issues
+
+ ### error: 1.2 Dictionaries to SceneKit.xcplaygroundpage:46:25: error: extraneous argument label 'node:' in call
+ ### let mySelection = select(node:scene.rootNode)
+
+ This is caused by SceneGraph not being compiled, and lldb simply skipping it.
+
+ Check that your Scheme is set to the correct Adaptor - probably StarJoinSceneKitAdaptor, and retry.
+
+ Note that when xcode resets, the scheme is often re-set to the default, which might not be the one you want.
+
+
+
+ */
--- StarJoinSpriteKitAdaptor/README.md
+++ StarJoinSpriteKitAdaptor/README.md
@@ -1,15 +1,3 @@
 # StarJoinSpriteKitAdaptor
 
 A description of this package.
-
-# TroubleShooting Playgrounds: Issues
-
-### error: 1.2 Dictionaries to SceneKit.xcplaygroundpage:46:25: error: extraneous argument label 'node:' in call
-### let mySelection = select(node:scene.rootNode)
-
-This is caused by SceneGraph not being compiled, and lldb simply skipping it.
-
-Check that your Scheme is set to the correct Adaptor - probably StarJoinSceneKitAdaptor, and retry.
-
-Note that when xcode resets, the scheme is often re-set to the default, which might not be the one you want.
-
